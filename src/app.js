81e1f1b26b0f75780748dc18a01971d0f8b58b0f
Felipe Campara <fcamparasilva@gmail.com>
2019-09-21 19:59:40 -0400

    Feat (Cors): add cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import routes from './routes'
 import './database'
 
@@ -11,12 +12,8 @@ class App {
   }
 
   middlewares () {
+    this.server.use(cors())
     this.server.use(express.json())
-    this.server.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*')
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-      next()
-    })
   }
 
   routes () {
