f88d990c4202ecbc516cf0c9288c22c4b59fa5ad
Felipe Campara <fcamparasilva@gmail.com>
2019-09-23 08:39:41 -0400

    Fix (Session): Change test create user to all tests

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -4,47 +4,41 @@ import app from '../../src/app.js'
 import factory from '../factories'
 import truncate from '../util/truncate.js'
 
-let user = null
 describe('Session', () => {
 
   beforeEach(async () => {
     await truncate()
-
-    user = await factory.attrs('User')
-    await request(app)
-      .post('/api/users')
-      .expect(200)
-      .send(user).then(({ body }) => {
-        expect(Object.keys(body).sort()).toEqual(['name', 'email', 'id'].sort())
-      })
   })
 
   it ('should be login success', async () => {
+    const { email, password } = await factory.create('User')
+
     await request(app)
       .post('/api/sessions')
       .expect(200)
-      .send({
-        email: user.email,
-        password: user.password
-      }).then(({ body }) => {
+      .send({ email, password })
+      .then(({ body }) => {
         expect(Object.keys(body).sort()).toEqual(['token', 'user'].sort())
       })
   })
 
   it ('should be login and failed because not exist user', async () => {
+    const { password } = await factory.create('User')
+
     const response = await request(app)
       .post('/api/sessions')
       .expect(401)
-      .send({ email: 'WRONG@WRONG.COM', password: user.password })
+      .send({ email: 'WRONG@WRONG.COM', password })
 
     expect(response.text).toEqual('{"success":false,"errors":["User not found"]}')
   })
 
   it('should be login and failed because password dont match', async () => {
+    const { email } = await factory.create('User')
     const response = await request(app)
       .post('/api/sessions')
       .expect(401)
-      .send({ email: user.email, password: 'WRONGPASSWORD' })
+      .send({ email, password: 'WRONGPASSWORD' })
 
     expect(response.text).toEqual('{"success":false,"errors":["Password does not match"]}')
   })
