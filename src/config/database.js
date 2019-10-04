ed67d47eb41c0b3a3894ece3041f75b73c9987ca
Felipe Campara <fcamparasilva@gmail.com>
2019-10-04 08:53:20 -0400

    fix dtabase config

--- src/config/database.js
+++ src/config/database.js
@@ -2,13 +2,20 @@
 require('../bootstrap')
 
 const database = process.env.NODE_ENV === 'production' ? {
-  use_env_variable: 'DATABASE_URL',
   dialect: process.env.DB_DIALECT || 'postgres',
-  ssl: true,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   dialectOptions: {
     ssl: {
       require: true
     }
+  },
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
   }
 } : {
   dialect: process.env.DB_DIALECT || 'postgres',
